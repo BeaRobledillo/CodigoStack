e195c95ff93e77e15d40689fa7bec264d8afaa42
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-05-30 19:31:12 +0200

    first commit

---
+++ src/main/java/com/factoriaf5/codigostack/CodigostackApplication.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.codigostack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigostackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigostackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/factoriaf5/codigostack/CodigostackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factoriaf5.codigostack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigostackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
