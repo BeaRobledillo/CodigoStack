62466a0de0a7e6823c580be23a7122561292b174
BeitxuelaWEB <bearobledillogomez@gmail.com>
2022-06-02 16:18:36 +0200

    +user

--- src/main/java/com/factoriaf5/codigostack/user/User.java
+++ src/main/java/com/factoriaf5/codigostack/user/User.java
@@ -17,5 +17,15 @@ public class User implements Serializable, UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String username;
+    private String password;
+    @Email
+    private String email;
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
