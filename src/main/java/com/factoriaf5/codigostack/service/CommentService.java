d17ada6b67df057bb5ed3bf41e0eb351612aa7b9
pruizcas <pruizcas@gmail.com>
2022-06-13 10:08:02 +0200

    second commit

--- src/main/java/com/factoriaf5/codigostack/service/CommentService.java
+++ src/main/java/com/factoriaf5/codigostack/service/CommentService.java
@@ -2,8 +2,11 @@ package com.factoriaf5.codigostack.service;
 
 import com.factoriaf5.codigostack.model.Comment;
 import com.factoriaf5.codigostack.repository.CommentRepository;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -13,5 +16,14 @@ import java.util.List;
 public class CommentService {
     private final CommentRepository CommentRepository;
 
-    public List<Comment> getAllComments() {return CommentRepository.findAll();};
+    public List<Comment> getAllComments() {return CommentRepository.findAll();}
+
+    public Comment findById (Long id) {
+        return CommentRepository.findById(id).orElse(null);
+    }
+
+    public Comment createComment (Comment comment) { return CommentRepository.save(comment); }
+
+    public Comment delete(Long id) {CommentRepository.deleteById(id); return null;}
+
 }
--- src/main/java/com/factoriaf5/codigostack/service/PostService.java
+++ src/main/java/com/factoriaf5/codigostack/service/PostService.java
@@ -2,8 +2,11 @@ package com.factoriaf5.codigostack.service;
 
 import com.factoriaf5.codigostack.model.Post;
 import com.factoriaf5.codigostack.repository.PostRepository;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -16,11 +19,25 @@ public class PostService {
         return  postRepository.findAll();
     }
 
+    public Post findById (Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public Post createPost (Post post) { return postRepository.save(post); }
+
+    public Post delete(Long id) {postRepository.deleteById(id); return null;}
+
+
+
+
+
+
+
     /*
-        1. crear un post
-        2. buscar un post por id
-        3. borrar un post
-        4. modificar un post
+        1. crear un post **
+        2. buscar un post por id **
+        3. borrar un post **
+        4. modificar un post **
      */
 
 }
